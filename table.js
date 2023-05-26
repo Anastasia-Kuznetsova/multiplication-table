89707dd0d6cda9f7525aed1f607e168b0fac9d41
Anastasia Kuznetsova <125804284+Anastasia-Kuznetsova@users.noreply.github.com>
2023-05-26 12:34:13 +0500

    Add files via upload

---
+++ table.js
@@ -0,0 +1,11 @@
+var newTable=document.createElement("table");
+newTable.align = "center";
+for (let i = 0; i < 10; i++){
+	var newRow = newTable.insertRow(i);
+	for(let j = 0; j < 10; j++){
+		var newCell = newRow.insertCell(j);
+		newCell.width = "10";
+		newCell.innerHTML = (i + 1) * (j + 1);
+	}
+}
+document.getElementById("printBlock").appendChild(newTable);
